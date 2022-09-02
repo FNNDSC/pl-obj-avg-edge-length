1ecd98198664284f2677f1a7d35470db45350079
Jennings Zhang <jenni_zh@protonmail.com>
2022-09-02 04:26:17 -0400

    Easy

--- edgy.py
+++ edgy.py
@@ -4,7 +4,10 @@ from pathlib import Path
 from argparse import ArgumentParser, Namespace, ArgumentDefaultsHelpFormatter
 from importlib.metadata import Distribution
 
-from chris_plugin import chris_plugin
+import numpy as np
+
+from bicpl import PolygonObj
+from chris_plugin import chris_plugin, PathMapper
 
 __pkg = Distribution.from_name(__package__)
 __version__ = __pkg.version
@@ -12,24 +15,49 @@ __version__ = __pkg.version
 
 parser = ArgumentParser(description='Average edge length about each vertex of a surface mesh.',
                         formatter_class=ArgumentDefaultsHelpFormatter)
-parser.add_argument('-n', '--name', default='foo',
-                    help='argument which sets example output file name')
 parser.add_argument('-V', '--version', action='version',
                     version=f'%(prog)s {__version__}')
+parser.add_argument('-f', '--filter', type=str, default='**/*.obj',
+                    help='input file filter')
+parser.add_argument('-s', '--suffix', type=str, default='.edgelen.txt',
+                    help='output file suffix')
 
 
 # documentation: https://fnndsc.github.io/chris_plugin/chris_plugin.html#chris_plugin
 @chris_plugin(
     parser=parser,
     title='Average Edge Length',
-    category='',                 # ref. https://chrisstore.co/plugins
+    category='Quality Control',  # ref. https://chrisstore.co/plugins
     min_memory_limit='100Mi',    # supported units: Mi, Gi
     min_cpu_limit='1000m',       # millicores, e.g. "1000m" = 1 CPU core
-    min_gpu_limit=0              # set min_gpu_limit=1 to enable GPU
 )
 def main(options: Namespace, inputdir: Path, outputdir: Path):
-    output_file = outputdir / f'{options.name}.txt'
-    output_file.write_text('did nothing successfully!')
+    mapper = PathMapper.file_mapper(inputdir, outputdir, glob=options.filter, suffix=options.suffix)
+    for input_file, output_file in mapper:
+        process(input_file, output_file)
+
+
+def process(input_file: Path, output_file: Path):
+    data = average_lengths(input_file)
+    mean = np.mean(data)
+    std = np.std(data)
+    np.savetxt(output_file, data)
+    print(f'{input_file} -> {output_file}', end=' ')
+    print(f'(min={np.min(data):.4f} max={np.max(data):.4f} mean={mean:.4f} std={std:.4f})')
+
+
+def average_lengths(filename: Path) -> list[float]:
+    obj = PolygonObj.from_file(filename)
+    return [
+        average_length_around(obj, index, neighbors)
+        for index, neighbors in enumerate(obj.neighbor_graph())
+    ]
+
+
+def average_length_around(obj: PolygonObj, index: int, neighbors: tuple[set[int]]) -> float:
+    coord = obj.point_array[index]
+    l = [np.linalg.norm(coord - obj.point_array[n]) for n in neighbors]
+    return np.mean(l)  # noqa: type
 
 
 if __name__ == '__main__':
